6a8c5ead60804a8e8cee7d9904ddf70659184648
Dustin Cintron <D.Cint88@gmail.com>
2019-06-10 11:14:12 -0400

    Debugging heroku2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://cintron-freelance.herokuapp.com/'
+    origins 'https://cintron-freelance.herokuapp.com/documents'
 
     resource '*',
       headers: :any,
